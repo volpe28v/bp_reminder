580bc5486a7dc41f69e952da09a0e63ec4e5a4a9
volpe28v <naoki.koda@gmail.com>
2021-10-20 10:31:37 +0900

    テキストが取得できた

--- src/index.ts
+++ src/index.ts
@@ -2,24 +2,41 @@ import { Client, LogLevel } from "@notionhq/client";
 import {
     GetPageParameters,
     GetPageResponse,
+    ListBlockChildrenResponse,
 } from "@notionhq/client/build/src/api-endpoints";
+import { exit } from "process";
+import { isNativeError } from "util/types";
+
+import slack from './slack';
 
 
 const notion = new Client({ auth: process.env.NOTION_ACCESS_TOKEN, logLevel: LogLevel.DEBUG });
-console.log(process.env.NOTION_ACCESS_TOKEN)
 
-const page_id = 'd967f950237f4b239a9a40fff87ca7c8'
+const pageId = process.env.NOTION_BP_PAGE_ID || '';
 const params: GetPageParameters = {
-    page_id: page_id
+    page_id: pageId
 }
 
 async function main() {
-    const page: GetPageResponse = await notion.pages.retrieve(params);
-    console.log("hello notion!")
-    console.log(page.properties)
-        
-    const postContent = await notion.blocks.children.list({ block_id: page_id });
-    console.dir(postContent, { depth: null });
+    const page: ListBlockChildrenResponse = await notion.blocks.children.list({ block_id: pageId});
+    console.dir(page, { depth: null });
+
+    const texts = page.results
+      .map((r) => {
+          if (r.type == 'bulleted_list_item' && r.bulleted_list_item.text[0].type == 'text'){
+            return r.bulleted_list_item.text[0].text.content;
+          }else{
+            return undefined;
+          }
+        })
+      .filter((r) => { return r != undefined})
+    
+    const text = texts[Math.floor(Math.random() * texts.length)];
+    console.log(text);
+
+    const sendMessage = `今日のベスプラ！ -> 『${text}』`
+    var slackClient = new slack;
+    slackClient.post({text: sendMessage}, function(e, r, b){ console.log('sent to slack!!')});
 }
 
 main();
---
+++ src/slack.ts
@@ -0,0 +1,28 @@
+import request = require('request')
+import http = require('http')
+
+export interface SlackPostForm {
+    token?: string
+    channel?: string
+    username?: string
+    text?: string
+}
+
+export default class Slack {
+    constructor() {
+    }
+
+    post(form: SlackPostForm, callBack: (error: any, response: http.IncomingMessage, body: any) => void) {
+        let options: request.CoreOptions = {
+            form: {
+                token: form.token || process.env.BP_REMINEDER_TOKEN,
+                channel: form.channel || 'z_times_kodama',
+                username: form.username || 'kani',
+                text: form.text || ''
+            }
+        }
+        request.post('https://slack.com/api/chat.postMessage', options, (error, response, body) => {
+            callBack(error, response, body)
+        })
+    }
+}
